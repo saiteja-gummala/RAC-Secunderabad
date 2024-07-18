169493d454cf777d96d68b37835a5d9ad8619d32
Gummala Saiteja <79400244+saiteja-gummala@users.noreply.github.com>
2024-07-19 01:00:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+function loadingAnimation() {
+  var tl = gsap.timeline();
+  tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+  });
+  tl.from("#line1-part1", {
+    opacity: 0,
+    onStart: function () {
+      var h5timer = document.querySelector("#line1-part1 h5");
+      var grow = 1962;
+      setInterval(function () {
+        if (grow < 2024) {
+          h5timer.innerHTML = grow++;
+        } else {
+          h5timer.innerHTML = grow;
+        }
+      }, 27);
+    },
+  });
+  tl.to(".line h2", {
+    animationName: "loaderAnime",
+    opacity: 1,
+  });
+  tl.to("#loader", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 2.6,
+  });
+  tl.from("#page1", {
+    delay: 0.1,
+    y: 1600,
+    duration: 0.5,
+    ease: Power4,
+  });
+  tl.to("#loader", {
+    display: "none",
+  });
+  tl.from("#nav", {
+    opacity: 0,
+  });
+  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
+    y: 140,
+    stagger: 0.2,
+  });
+  tl.from(
+    "#hero1, #page2",
+    {
+      opacity: 0,
+    },
+    "-=1.2"
+  );
+}
+
+// Toggle mobile menu
+const menuToggle = document.getElementById("menu-toggle");
+const menuClose = document.getElementById("menu-close");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuToggle.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
+menuClose.addEventListener("click", () => {
+  mobileMenu.classList.add("hidden");
+});
+
+loadingAnimation();
+
+// ProgressBar
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+};
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+// Hero animation
+let tl = gsap.timeline();
+tl.from(".hero-background p",{
+  y:-500,
+  opacity:0,
+  delay:5,
+  duration:0.6,
+  stagger:0.3
+})
+tl.from(".hero-background h1",{
+  x:-500,
+  opacity:0,
+  duration:1,
+  stagger:0.3
+})
+
+
